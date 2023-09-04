6afe43902ca3961d5d43d68227df062615601f49
しゃかな 沖山 <okiyama1609@yahoo.co.jp>
2023-09-04 16:17:51 +0900

    こめついか

--- Input.h
+++ Input.h
@@ -6,10 +6,22 @@ public:
 
 	static Input* GetInstance();
 
+	/// <summary>
+	/// キー更新
+	/// </summary>
 	static void InputUpdate();
 
+	/// <summary>
+	/// 押し続けた時
+	/// </summary>
 	static bool GetKey(int key);
+	/// <summary>
+	/// 押した時
+	/// </summary>
 	static bool GetTriggerKey(int key);
+	/// <summary>
+	/// 離した時
+	/// </summary>
 	static bool GetReleaseKey(int key);
 	
 private:
