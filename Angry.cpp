bbd5ccf1fb536429dc81d70a54e7273fe9859860
IijimaKaoru <k021g1465@g.neec.ac.jp>
2023-09-10 09:15:46 +0900

    ミス演出作成中

---
+++ Angry.cpp
@@ -0,0 +1,23 @@
+#include "Angry.h"
+
+Angry* Angry::Create(const int& texNum_, const Vector2& pos_, const Vector2& velocity_)
+{
+    return nullptr;
+}
+
+void Angry::Init(const int& texNum_, const Vector2& pos_, const Vector2& velocity_)
+{
+}
+
+void Angry::Update()
+{
+}
+
+void Angry::Draw()
+{
+}
+
+const bool Angry::GetIsDead() const
+{
+    return isDead;
+}
