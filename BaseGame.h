99e476eea17a554654c0ef602f7b95469082b81e
shakana <okiyama1609@yahoo.co.jp>
2024-09-08 21:40:27 +0900

    push

--- BaseGame.h
+++ BaseGame.h
@@ -10,8 +10,18 @@ public:
 	virtual void Update() = 0;
 	virtual void Draw() = 0;
 
+	int GetLayer() {
+		return layer_;
+	}
+
+	bool GetEnd() {
+		return end_;
+	}
+
 protected:
 	int layer_ = 0;
 
 	BoxTransform window_;
+	int layer_ = 50;
+	bool end_ = false;
 };
