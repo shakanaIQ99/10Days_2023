a8181330333ef39b0794d8b8f35fe89800c75553
IijimaKaoru <k021g1465@g.neec.ac.jp>
2024-09-09 10:24:42 +0900

    ウィンドウ(ミニ)大きさ調整

--- HelpSunGame.cpp
+++ HelpSunGame.cpp
@@ -6,17 +6,18 @@ void HelpSunGame::Init()
 	input_ = Input::GetInstance();
 
 	window_.pos = { WIN_WIDTH / 2,WIN_HEIGHT / 2 };
-	window_.width = WIN_WIDTH * 1 / 7;
-	window_.height = WIN_HEIGHT * 1 / 7;
+	window_.width = WIN_WIDTH * 1 / 3;
+	window_.height = WIN_HEIGHT * 1 / 3;
 
 	sun_.pos = window_.pos;
-	sun_.width = 60;
-	sun_.height = 60;
+	sun_.width = 120;
+	sun_.height = 120;
 
 	for (size_t i = 0; i < CLOUD_NUM; i++)
 	{
-		clouds_[i].pos = { sun_.pos.x + Util::GetRand(-30.0f,30.0f),sun_.pos.y + Util::GetRand(-30.0f,30.0f) };
-		clouds_[i].width = Util::GetRand(25.0f, 50.0f);
+		clouds_[i].pos = { sun_.pos.x + Util::GetRand(-sun_.width / 2,sun_.width / 2),
+			sun_.pos.y + Util::GetRand(-sun_.height / 2,sun_.height / 2) };
+		clouds_[i].width = Util::GetRand(50.0f, 100.0f);
 		clouds_[i].height = clouds_[i].width - 10.0f;
 		isClouds_[i] = true;
 	}
