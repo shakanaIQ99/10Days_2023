2612da3708edd157971d0cbf43917e53c62e6270
IijimaKaoru <k021g1465@g.neec.ac.jp>
2024-09-13 11:26:09 +0900

    一旦

--- ZihannGame.cpp
+++ ZihannGame.cpp
@@ -1,1 +1,28 @@
 #include "ZihannGame.h"
+
+ZihannGame::ZihannGame(int layernum, const Vector2& pos)
+{
+	input_ = Input::GetInstance();
+
+	layer_ = layernum;
+
+	topBar_.width = 420;
+	topBar_.height = 32 * 3 / 2;
+	topBar_.pos = pos;
+
+	window_.width = 420;
+	window_.height = 240;
+	window_.pos = { topBar_.pos.x, topBar_.pos.y + (topBar_.height / 2 + window_.height / 2) };
+}
+
+ZihannGame::~ZihannGame()
+{
+}
+
+void ZihannGame::Update()
+{
+}
+
+void ZihannGame::Draw()
+{
+}
--- ZihannGame.h
+++ ZihannGame.h
@@ -1,6 +1,27 @@
 #pragma once
 #include "BaseGame.h"
+#include "Input.h"
+
 class ZihannGame : public BaseGame
 {
+public:
+	enum Phase {
+		ZihannMae,
+		ZihannSita
+	};
+
+public:
+	ZihannGame(int layernum, const Vector2& pos);
+	~ZihannGame();
+	void Update() override;
+	void Draw() override;
+
+private:
+	Input* input_ = nullptr;
+
+	Phase phase_ = ZihannMae;
+
+	BoxTransform zihannki_;
+	Vector2 nowZihannki_;
 };
 
